58e850c951b5c75923e80e0186aaf4382889c3a4
Nasrin200 <nasrin.sama@gmail.com>
2022-03-07 23:39:03 +0330

    transfared codes from SandBox to VsCode

---
+++ src/Weather.js
@@ -0,0 +1,42 @@
+import React from "react";
+import "./App.css"
+
+export default function Weather() {
+  return (
+    <div className="Weather">
+      <span>
+        <form class="weather" id="forms">
+          <input type="text" id="cityname" class="cityName" />
+          <input
+            class="search"
+            type="submit"
+            name="weather"
+            id="weather"
+            value="Search"
+          />
+        </form>
+      </span>
+      <button>Current</button>
+      <div class="header">
+        weather in
+        <h1 id="hc">new york</h1>
+        now
+      </div>
+      <p id="time">Current time: 10:00</p>
+      <div class="sunny">
+        <span>
+          <h2 id="cityTemp">
+            10<span id="centigrad"> °C|°F</span>
+          </h2>
+          <span class="sun">☀</span>
+        </span>
+
+        <div>
+          Precipitation:<span id="prec">0</span>% <br />
+          Humidity: <span id="hum">14</span>% <br />
+          Wind: <span id="wind">8</span>km/h
+        </div>
+      </div>
+    </div>
+  );
+}
